9645cfbb6edab63f1804848190dcdc04ce731f44
pbjohn1978 <pbjohn1978@yahoo.com>
2017-12-07 14:58:54 -0800

    Update Gnax65VAkz79W88lpTgE.php
    updates for url minup

--- sysxygh/classes/Gnax65VAkz79W88lpTgE.php
+++ sysxygh/classes/Gnax65VAkz79W88lpTgE.php
@@ -1,7 +1,16 @@
 <?php
-	
-	require_once('./zSntUkOYX5MGUGz7duv3.php');
+    require_once('./zSntUkOYX5MGUGz7duv3.php');
     require_once('./mpT7nJTHkLFr5GK4jdXo.php');
+    require_once('./ava3KsVUvw8syHCJpitM.php');
+    session_start();
+    $userGuid = '';
+    if( isset($_SESSION['userGuid']) ){
+        $userGuid = $_SESSION['userGuid'];
+    }
+    $login = new sessionHelper;
+    if( !($login->{'isUserLoggedIn'}()) ){
+	    header('Location: ../home.php?message=Did You Reach This Page In Error?');
+    }
     
 	if( isset($_GET["userToDelete"]) ) {
 		$mydb = new dbHelper;
