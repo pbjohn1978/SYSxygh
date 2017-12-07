1b592ad48928ae07db96e5fc1640f2f560f314ce
John Chandler <chandler6830@student.cptc.edu>
2017-12-06 18:36:35 -0800

    fixed the broken installer... :)

--- InstallSYSxygh.py
+++ InstallSYSxygh.py
@@ -17,16 +17,16 @@ else:
 	print("there is a problem with starting Apache, please turn it on manually than rerun this script.")
 
 #Chmod files that need it for website scripts
-file1 = os.path.isfile("/var/www/html/sysxygh/classes/bTRzRFCqrqiVKTYh8TtQ.sh")
-file2 = os.path.isfile("/var/www/html/sysxygh/classes/Rme6jc7n7zpCMJLprV1X.py")
-file3 = os.path.isfile("/var/www/html/sysxygh/classes/w3Y1anVD77Qoki2Cdvnn.py")
-dire1 = os.path.isdir("/var/www/html/sysxygh")
+file1 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/classes/bTRzRFCqrqiVKTYh8TtQ.sh")
+file2 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/classes/Rme6jc7n7zpCMJLprV1X.py")
+file3 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/classes/w3Y1anVD77Qoki2Cdvnn.py")
+dire1 = os.path.isdir("/var/www/html/SYSxygh/sysxygh")
 
 if(file1 & file2 & file3 & dire1):
-	if(os.system("sudo chmod 777 /var/www/html/sysxygh/classes/bTRzRFCqrqiVKTYh8TtQ.sh") == 0 &
-	   os.system("sudo chmod 777 /var/www/html/sysxygh/classes/Rme6jc7n7zpCMJLprV1X.py") == 0 &
-	   os.system("sudo chmod 777 /var/www/html/sysxygh/classes/w3Y1anVD77Qoki2Cdvnn.py") == 0 &
-	   os.system("sudo chmod 777 /var/www/html/sysxygh")):
+	if(os.system("sudo chmod 777 /var/www/html/SYSxygh/sysxygh/classes/bTRzRFCqrqiVKTYh8TtQ.sh") == 0 &
+	   os.system("sudo chmod 777 /var/www/html/SYSxygh/sysxygh/classes/Rme6jc7n7zpCMJLprV1X.py") == 0 &
+	   os.system("sudo chmod 777 /var/www/html/SYSxygh/sysxygh/classes/w3Y1anVD77Qoki2Cdvnn.py") == 0 &
+	   os.system("sudo chmod 777 /var/www/html/SYSxygh/sysxygh")):
 		print("Files have nessary permitions...")
 	else:
 		print("There has been an error granting permitions to the nessary SYSxygh files.")
@@ -34,7 +34,7 @@ else:
 	print("the nessary files for SYSxygh have not been put in the /var/www/html directory... ")
 	print("please copy the SYSxygh files to /var/www/html than run this script again...")
 
-#
+#creating the Database for SYSxygh
 db = mdb.connect()
 c = db.cursor()
 c.execute("DELETE FROM mysql.user WHERE user='sysxyghWebGUI'")
@@ -43,7 +43,21 @@ c.execute("CREATE DATABASE sysxygh;")
 c.execute("GRANT ALL PRIVILEGES ON sysxygh.* to sysxyghWebGUI@'%' IDENTIFIED BY 'YixkmYBVQO88BaqF5dGNyC9xbt5DD1VmWYkrESMfJvfqPtnKvxkInhPiut45';")
 c.close()
 
-os.system("sudo mysql -u sysxyghWebGUI -pYixkmYBVQO88BaqF5dGNyC9xbt5DD1VmWYkrESMfJvfqPtnKvxkInhPiut45 sysxygh < /var/www/html/sysxygh.sql") 
+os.system("sudo mysql -u sysxyghWebGUI -pYixkmYBVQO88BaqF5dGNyC9xbt5DD1VmWYkrESMfJvfqPtnKvxkInhPiut45 sysxygh < /var/www/html/SYSxygh/sysxygh.sql") 
+
+file4 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/sysxygh.jsproj")
+file5 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/sysxygh.jsproj.usr")
+file6 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/sysxygh.sln")
+file7 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/sysxygh.sql")
+file8 = os.path.isfile("/var/www/html/SYSxygh/sysxygh/taco.json")
+
+if(file4 & file5 & file6 & file7 & file8):
+    os.system("sudo rm /var/www/html/SYSxygh/sysxygh/sysxygh.jsproj")
+    os.system("sudo rm /var/www/html/SYSxygh/sysxygh/sysxygh.jsproj.usr")
+    os.system("sudo rm /var/www/html/SYSxygh/sysxygh/sysxygh.sln")
+    os.system("sudo rm /var/www/html/SYSxygh/sysxygh/sysxygh.sql")
+    os.system("sudo rm /var/www/html/SYSxygh/sysxygh/taco.json")
+
 
 print("")
 print("")
@@ -53,7 +67,7 @@ ipAddress = s.getsockname()[0]
 s.close()
 
 print( 'Your IP is: ' + str(ipAddress) )
-print('Place this url in your favorite browser: http://' + str(ipAddress) + '/sysxygh/home.php')
+print('Place this url in your favorite browser: http://' + str(ipAddress) + '/SYSxygh/sysxygh/home.php')
 print("")
 print("username: admin")
 print("password: sysxygh")
